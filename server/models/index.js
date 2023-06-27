96d4d7b4bb7fca5416bd1dc0c28a7fa8c8fde2d2
Jaimon <jamin.hogan@gmail.com>
2023-06-26 21:45:30 -0400

    updated models/index and added models/Club

--- server/models/index.js
+++ server/models/index.js
@@ -2,5 +2,6 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
+const Club = require('./Club');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Product, Category, Order, Club };
