f0ef29ea4c73f423b75759068254039866ebdbc0
Jaimon <jamin.hogan@gmail.com>
2023-06-29 16:27:20 -0400

    added helper functions and model and schemas/TypeDefs for geocodes

--- client/src/components/EventMap/index.js
+++ client/src/components/EventMap/index.js
@@ -1,21 +1,53 @@
 import {useCallback, useState} from "react"; //
 import { GoogleMap, useJsApiLoader, Marker} from "@react-google-maps/api"; //
 
+// import fetch from "node-fetch"; // npm i node-fetch
+
+
  const containerStyle = {
     width: '300px',
     height: '300px'
  };
 
- const center = {
-    lat: 33.7722,
-    lng: -84.3902
- };
+//  const center = {
+//     lat: 33.7722,
+//     lng: -84.3902
+//  };
 
-export default function EventMap() {
+// let geoCode = {};
+
+//  function getGeocode(address) {
+//     const geoArray = address.trim().split(' ');
+//     const geoString = geoArray.join("+");
+//     console.log("geoString is:", geoString);
+//     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${geoString}&key=AIzaSyDRsmW5OJbpiAhShk3u3a06FwJK9F9RjE4`)
+//     .then((response) => {
+//         return response.json();
+//     }).then(jsonData => {
+//         geoCode = jsonData.results[0].geometry.location;
+//         console.log('geoCode is', geoCode);
+//         return geoCode;
+
+//     })
+//     .catch(err => {
+//         throw new Error(err);
+//     })
+//  }
+
+export default function EventMap(center) {
+    // hardcoded overwrite for test purposes
+    center = { 
+        lat: 33.7722,
+        lng: -84.3902
+    };
+
+    // const address = "1837 Glendmere Drive, Birmingham, AL 35223";
+    // geoCode = geocode(address)
+    // console.log("geoCode in EventMap is", geoCode)
 
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
-        googleMapsApiKey: "" // add Key from Slack
+        googleMapsApiKey: "AIzaSyDRsmW5OJbpiAhShk3u3a06FwJK9F9RjE4" // add Key from Slack
         
     });
     //process.env.REACT_APP_GOOGLE_MAPS_API_KEY // not secure
@@ -46,6 +78,8 @@ export default function EventMap() {
             ></GoogleMap>)
             : <> </>
     )
+
 } 
 
 
+// getGeocode("1837 Glendmere Drive, Birmingham, AL 35223");
