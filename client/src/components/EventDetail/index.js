75d5a7dd976d5fce83eae6be1d7a18f07c712f04
Anabel-Espinoza <anabel.espinoza@gmail.com>
2023-06-30 21:36:54 -0400

    Added details to event by ID

--- client/src/components/EventDetail/index.js
+++ client/src/components/EventDetail/index.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
-
 import { Link, useParams } from "react-router-dom";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_EVENT, QUERY_ME } from "../../utils/queries";
 import { JOIN_EVENT } from "../../utils/mutations";
 import Auth from "../../utils/auth";
+let signedUp = true    
 
 const EventDetail = () => {
 
-    // const [addEvent, { error }] = useMutation(JOIN_EVENT);
+    const [joinEvent, { error }] = useMutation(JOIN_EVENT);
+    // const [signedUp, setSignedUp] = useState(false)
 
     const { id: eventId } = useParams();
     console.log(eventId)
@@ -22,16 +23,19 @@ const EventDetail = () => {
 
     const userData = meData?.me || {};
 
+    let myEventsId = []
+    for (let i=0; i < userData.myEvents?.length; i++) {
+        myEventsId.push(userData.myEvents[i]._id)
+    }
+    console.log(eventData)
 
     // Check if user has already sign for the event
-    //     let myEventsID = userData.myEvents.id
-    //     console.log(myEventsID)
-    //     if (myEventsID.contain(eventData._id)) {
-    //         setJoinedEvent(true)
-
-    console.log(userData)
-    console.log(eventData)
-    // console.log(joinedEvent)
+    if (myEventsId.includes(eventId)) {
+        // setSignedUp(true)
+        signedUp = true    
+    }
+        console.log(signedUp)
+    
     if (loading || meLoading) {
         return <div>Loading...</div>
     }
@@ -55,20 +59,47 @@ const EventDetail = () => {
     // };
 
 
-
     return (
         <div>
-            <h3>{eventData.title}</h3>
-            <p>{eventData.description}</p>
-            {token ? (
+            <div key={eventData._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+              { !eventData.image ? (
+                <img className="w-full" 
+                    src= './images/event_default.jpg' />
+                ) : (
+                    <img className="w-full" 
+                    src= {eventData.image} />
+                )}
+                <div className="px-6 py-4">
+                    <div className='font-bold text-xl mb-2'>
+                    {eventData.title}
+                </div>
+                <p className='text-gray-700 text-base'>
+                {eventData.description}
+                </p>
+                <ul className='text-gray-700 text-base'>
+                    <li>Date and Time: {eventData.dateTime}</li>
+                    <li>{eventData.address}</li>
+                    {eventData.isAvailable 
+                        ? <li>Available</li>
+                        : <li>Not Available</li>}
+                </ul>
+            </div>
+             {token ? (
+                signedUp ? (
+                    <p>You're already signed up for his event!</p>
+                    ) : (
                 <button
                     className="bg-red-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => { handleJoinEvent(true) }}
-                >Join Event </button>
-            ) : null}
+                    >Join Event 
+                </button>
+                )
+                ) : 
+                <p>Log in to Join!</p>}
 
         </div>
+    </div>
     )
 }
 
